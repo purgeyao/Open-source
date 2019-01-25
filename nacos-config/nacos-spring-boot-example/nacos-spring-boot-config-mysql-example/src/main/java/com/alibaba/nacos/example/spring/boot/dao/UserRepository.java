791b842eb0d4e851750345c35f60122615368078
zhaoyao <15138067756@163.com>
2019-01-25 23:04:45 +0800

    delete project

--- nacos-config/nacos-spring-boot-example/nacos-spring-boot-config-mysql-example/src/main/java/com/alibaba/nacos/example/spring/boot/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.alibaba.nacos.example.spring.boot.dao;
-
-import com.alibaba.nacos.example.spring.boot.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
